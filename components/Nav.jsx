811ae5679ce13d9e5aedb910309532795e6a885b
Kevin <fcclea@gmail.com>
2025-07-12 07:32:48 -0400

    first commit

---
+++ components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Nav() {
+  return (
+    <nav>
+      <div className="nav__container">
+        <a href="/">
+          <img src="" alt="" className="logo" />
+        </a>
+        <ul className="nav__links">
+          <li className="nav__list">
+            <a href="/" className="nav__link">
+              Books
+            </a>
+          </li>
+          <button className="btn__menu">
+            <FontAwesomeIcon icon="bars" />
+          </button>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav
