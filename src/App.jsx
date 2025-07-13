d229a8803977ee357877464f2088d5e2151e8ee4
Kevin <fcclea@gmail.com>
2025-07-13 06:20:51 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Nav from './components/Nav';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
     </div>
   );
 }
